a9a097edb7a82d6a546a3073d4fce24425c1d7a8
Karthik <meharkarthik9@gmail.com>
2026-03-08 18:05:15 +0530

    added registration page and updated login UI

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import hello_new
+from . import views
 
 urlpatterns = [
-    path('newhello/', hello_new),
+    path("register/", views.register, name="register"),
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -1,4 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-def hello_new(request):
-    return HttpResponse("Hello World from NEW APP from intlo!")
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect("register")
+
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        messages.success(request, "Account created successfully")
+        return redirect("/login/")
+
+    return render(request, "register.html")
